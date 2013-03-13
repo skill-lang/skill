2ec0f252904b94fe85f01d9c9827338d5271eb9e
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2013-03-13 14:17:49 +0100

    added pretty printing projection tests to ensure that multiple parser and pretty printing cycles preserve the content

---
+++ test/de/ust/skill/parser/ParserPrettyPrintingProjectionTest.scala
@@ -0,0 +1,38 @@
+package de.ust.skill.parser
+
+import java.io.File
+import org.junit.Test
+import org.scalatest.junit.AssertionsForJUnit
+import sys.process._
+import org.junit.Assert
+import org.junit.ComparisonFailure
+
+class ParserPrettyPrintingProjectionTest extends AssertionsForJUnit {
+
+  def check(path: String) = {
+    val p = new Parser
+    val first = p.process(new File(path)).toList
+    val tmp = File.createTempFile("test", "skill")
+    ("echo " + ASTPrettyPrinter.prettyPrint(first)) #> tmp !
+    val second = p.process(tmp).toList
+    tmp.delete()
+    ("echo " + ASTPrettyPrinter.prettyPrint(first)) #> tmp !
+    val third = p.process(tmp).toList
+    tmp.delete()
+    Assert.assertTrue(ASTEqualityChecker.checkDefinitionList(third, second))
+  }
+
+  @Test def test: Unit = check("test/data/test.skill")
+
+  @Test def hints: Unit = check("test/data/hints.skill")
+  @Test def test2: Unit = check("test/data/test2.skill")
+  @Test def test3: Unit = check("test/data/test3.skill")
+  @Test def example1: Unit = check("test/data/example1.skill")
+  @Test def example2a: Unit = check("test/data/example2a.skill")
+  @Test def example2b: Unit = check("test/data/example2b.skill")
+  @Test def unicode: Unit = check("test/data/unicode.skill")
+  @Test def airTop: Unit = check("test/data/air-top.skill")
+  @Test def airPamm: Unit = check("test/data/air-pamm.skill")
+  @Test def airHeap: Unit = check("test/data/air-pamm-heap.skill")
+
+}
