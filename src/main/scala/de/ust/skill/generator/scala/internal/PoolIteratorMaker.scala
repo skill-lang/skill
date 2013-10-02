dca6648bc5cd97697f8d1ff1eb5e0f06202bcee9
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2013-10-02 10:03:52 +0200

    updated file headers

--- src/main/scala/de/ust/skill/generator/scala/internal/PoolIteratorMaker.scala
+++ src/main/scala/de/ust/skill/generator/scala/internal/PoolIteratorMaker.scala
@@ -1,3 +1,8 @@
+/*  ___ _  ___ _ _                                                            *\
+** / __| |/ (_) | |       The SKilL Generator                                 **
+** \__ \ ' <| | | |__     (c) 2013 University of Stuttgart                    **
+** |___/_|\_\_|_|____|    see LICENSE                                         **
+\*                                                                            */
 package de.ust.skill.generator.scala.internal
 
 import de.ust.skill.generator.scala.GeneralOutputMaker
