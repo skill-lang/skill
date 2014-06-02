7308714f794a95466101e2f086ecd76b1a215fe4
Dennis Przytarski <dennis@przytarski.com>
2014-06-03 00:22:10 +0200

    changed documentation

--- src/main/scala/de/ust/skill/generator/ada/api/internal/FileReaderSpecMaker.scala
+++ src/main/scala/de/ust/skill/generator/ada/api/internal/FileReaderSpecMaker.scala
@@ -32,9 +32,9 @@ with Ada.Unchecked_Deallocation;
 --    d) it reads the field counter
 --    e) depending on the instance and field counter, it will continue with one of the three cases:
 --      -> case 1: if instance counter = 0 then assume new fields
---      -> case 2: if instance counter > 0 and field counter = 0 then assume new objects
---      -> case 3: if instance counter > 0 and field counter > 0 then assume new objects and after that new fields
---      => when fields are read, the field will be put into a 'read field data' queue
+--      -> case 2: if instance counter > 0 and new! field counter = 0 then assume new objects
+--      -> case 3: if instance counter > 0 and new! field counter > 0 then assume new objects and after that new fields
+--      => when fields are read, the field will be put into a field-data queue
 --    f) it reads the field data processing the queue from start to end
 --
 --  3. Update storage pool start index (spsi)
