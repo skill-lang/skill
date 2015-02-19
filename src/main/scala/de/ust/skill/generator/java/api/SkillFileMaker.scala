9365bc7166d769022e2e41313f3e60471c7cd2ee
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2015-02-19 17:52:20 +0100

    [java]fixed translation for simple examples

--- src/main/scala/de/ust/skill/generator/java/api/SkillFileMaker.scala
+++ src/main/scala/de/ust/skill/generator/java/api/SkillFileMaker.scala
@@ -13,95 +13,68 @@ import de.ust.skill.generator.java.GeneralOutputMaker
 trait SkillFileMaker extends GeneralOutputMaker {
   abstract override def make {
     super.make
-    val out = open("api/SkillFile.scala")
+    val out = open("api/SkillFile.java")
 
     //package & imports
-    out.write(s"""package ${packagePrefix}api
+    out.write(s"""package ${packagePrefix}api;
 
-import java.nio.file.Path
-import java.io.File
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
 
-import scala.collection.mutable.ArrayBuffer
-
-import _root_.${packagePrefix}internal.FileParser
-import _root_.${packagePrefix}internal.State
-import _root_.${packagePrefix}internal.SkillType
+import ${packagePrefix}internal.SkillState;
+import de.ust.skill.common.java.api.SkillException;
 
 /**
- * The public interface to the in-memory representation of a SKilL file.
- * This class provides access to instances of types stored in a file as well as state management capabilities.
- *
- * @note The well-formedness of a file can be checked at any time using the check() method
- * @todo changePath
- * @todo changeWriteMode
+ * An abstract skill file that is hiding all the dirty implementation details
+ * from you.
+ * 
  * @author Timm Felden
  */
-trait SkillFile {
-${
-      (for (t ← IR) yield s"  val ${name(t)} : ${name(t)}Access").mkString("\n")
+public interface SkillFile extends de.ust.skill.common.java.api.SkillFile {
+
+    /**
+     * Create a new skill file based on argument path and mode.
+     * 
+     * @throws IOException
+     *             on IO and mode related errors
+     * @throws SkillException
+     *             on file or specification consistency errors
+     */
+    public static SkillFile open(String path, Mode... mode) throws IOException, SkillException {
+        return SkillState.open(path, mode);
     }
 
-  val String : StringAccess
+    /**
+     * Create a new skill file based on argument path and mode.
+     * 
+     * @throws IOException
+     *             on IO and mode related errors
+     * @throws SkillException
+     *             on file or specification consistency errors
+     */
+    public static SkillFile open(File path, Mode... mode) throws IOException, SkillException {
+        return SkillState.open(path, mode);
+    }
 
-  def all : Iterator[Access[_ <: SkillType]]
+    /**
+     * Create a new skill file based on argument path and mode.
+     * 
+     * @throws IOException
+     *             on IO and mode related errors
+     * @throws SkillException
+     *             on file or specification consistency errors
+     */
+    public static SkillFile open(Path path, Mode... mode) throws IOException, SkillException {
+        return SkillState.open(path, mode);
+    }${
+      (for (t ← IR) yield s"""
 
-  /**
-   * changes output path
-   * @note in append mode, the old file will be copied to the new path; this may take some time
-   */
-  def changePath(path : Path) : Unit
-  /**
-   * change mode
-   * @note currently only append -> write is supported; if you want to change write -> append, you are probably looking
-   * for open(Create, Append) instead
-   */
-  def changeMode(writeMode : Mode) : Unit
-
-  /**
-   * Checks restrictions in types. Restrictions are checked before write/append, where an error is raised if they do not
-   * hold.
-   */
-  def check : Unit
-
-  /**
-   * Check consistency and write changes to disk.
-   * @note this will not sync the file to disk, but it will block until all in-memory changes are written to buffers.
-   * @note if check fails, then the state is guaranteed to be unmodified compared to the state before flush
-   */
-  def flush : Unit
-
-  /**
-   * Same as flush, but will also sync and close file, thus the state is not usable afterwards.
-   */
-  def close : Unit
-}
-
-/**
- * Modes for file handling.
- */
-sealed abstract class Mode;
-sealed abstract class OpenMode extends Mode;
-sealed abstract class WriteMode extends Mode;
-object Create extends OpenMode;
-object Read extends OpenMode;
-object Write extends WriteMode;
-object Append extends WriteMode;
-
-object SkillFile {
-
-  /**
-   * Reads a binary SKilL file and turns it into a SKilL state.
-   */
-  def open(path : Path, flags : Mode*) : SkillFile = State.open(path, flags.to)
-  def open(file : File, flags : Mode*) : SkillFile = State.open(file.ensuring(exists(_)).toPath, flags.to)
-  def open(path : String, flags : Mode*) : SkillFile = State.open(new File(path).ensuring(exists(_)).toPath, flags.to)
-
-  // ensures existence :)
-  private def exists(f : File) = {
-    if (!f.exists())
-      f.createNewFile()
-    true
-  }
+    /**
+     * @return an access for all ${name(t)} in this state
+     */
+    public ${packagePrefix}internal.${name(t)}Access ${name(t)}s();""").mkString("")
+    }
 }
 """)
 
--- src/main/scala/de/ust/skill/generator/java/internal/StateMaker.scala
+++ src/main/scala/de/ust/skill/generator/java/internal/StateMaker.scala
@@ -21,177 +21,206 @@ import de.ust.skill.ir.restriction.SingletonRestriction
 trait StateMaker extends GeneralOutputMaker {
   abstract override def make {
     super.make
-    val out = open("internal/State.scala")
+    val out = open("internal/SkillState.java")
 
-    out.write(s"""package ${packagePrefix}internal
+    out.write(s"""package ${packagePrefix}internal;
 
-import java.io.IOException
-import java.nio.file.Files
-import java.nio.file.Path
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
-import scala.collection.mutable.ArrayBuffer
-import scala.collection.mutable.HashSet
+import de.ust.skill.common.java.api.Access;
+import de.ust.skill.common.java.api.FieldDeclaration;
+import de.ust.skill.common.java.api.SkillException;
+import de.ust.skill.common.java.internal.BasePool;
+import de.ust.skill.common.java.internal.SkillObject;
+import de.ust.skill.common.java.internal.StoragePool;
+import de.ust.skill.common.java.internal.StringPool;
+import de.ust.skill.common.java.internal.fieldTypes.Annotation;
+import de.ust.skill.common.java.internal.fieldTypes.StringType;
+import de.ust.skill.common.jvm.streams.FileInputStream;
 
-import de.ust.skill.common.jvm.streams.FileInputStream
-import de.ust.skill.common.jvm.streams.FileOutputStream
-
-import _root_.${packagePrefix}api._
+import ${packagePrefix}api.SkillFile;
 
 /**
- * This class is used to handle objects in a serializable state.
- *
+ * Internal implementation of SkillFile.
+ * 
  * @author Timm Felden
+ * @note type access fields start with a capital letter to avoid collisions
  */
-final class State private[internal] (
-${
-      if (largeSpecificationMode) ""
-      else (for (t ← IR) yield s"  val ${name(t)} : ${name(t)}Access,").mkString("\n")
+public final class SkillState extends de.ust.skill.common.java.internal.SkillState implements SkillFile {
+
+    // types in type order
+    private ArrayList<StoragePool<?, ?>> types;
+
+    // types by skill name
+    private final HashMap<String, StoragePool<?, ?>> poolByName;
+
+    @Override
+    public HashMap<String, StoragePool<?, ?>> poolByName() {
+        return poolByName;
     }
-  val String : StringAccess,
-  val pools : Array[StoragePool[_ <: SkillType, _ <: SkillType]],
-  var path : Path,
-  var mode : WriteMode)
-    extends SkillFile {
 
-  val poolByName = pools.map(_.name).zip(pools).toSeq.toMap
-${
-      if (largeSpecificationMode) (for (t ← IR) yield s"""  val ${name(t)} = poolByName("${t.getSkillName}").asInstanceOf[${name(t)}Access]""").mkString("\n", "\n", "\n")
-      else ""
+    /**
+     * Create a new skill file based on argument path and mode.
+     * 
+     * @throws IOException
+     *             on IO and mode related errors
+     * @throws SkillException
+     *             on file or specification consistency errors
+     */
+    public static SkillState open(String path, Mode... mode) throws IOException, SkillException {
+        File f = new File(path);
+        assert f.exists() : "can only open files that already exist in genarel, because of java.nio restrictions";
+        return open(f, mode);
     }
-  finalizePools;
 
-  def all = pools.iterator.asInstanceOf[Iterator[Access[_ <: SkillType]]]
-
-  @inline private def finalizePools {
-    def eliminatePreliminaryTypesIn[T](t : FieldType[T]) : FieldType[T] = t match {
-      case TypeDefinitionIndex(i) ⇒ try {
-        pools(i.toInt).asInstanceOf[FieldType[T]]
-      } catch {
-        case e : Exception ⇒ throw new IllegalStateException(s"inexistent user type $$i (user types: $${poolByName.mkString})", e)
-      }
-      case TypeDefinitionName(n) ⇒ try {
-        poolByName(n).asInstanceOf[FieldType[T]]
-      } catch {
-        case e : Exception ⇒ throw new IllegalStateException(s"inexistent user type $$n (user types: $${poolByName.mkString})", e)
-      }
-      case ConstantLengthArray(l, t) ⇒ ConstantLengthArray(l, eliminatePreliminaryTypesIn(t))
-      case VariableLengthArray(t)    ⇒ VariableLengthArray(eliminatePreliminaryTypesIn(t))
-      case ListType(t)               ⇒ ListType(eliminatePreliminaryTypesIn(t))
-      case SetType(t)                ⇒ SetType(eliminatePreliminaryTypesIn(t))
-      case MapType(k, v)             ⇒ MapType(eliminatePreliminaryTypesIn(k), eliminatePreliminaryTypesIn(v))
-      case t                         ⇒ t
+    /**
+     * Create a new skill file based on argument path and mode.
+     * 
+     * @throws IOException
+     *             on IO and mode related errors
+     * @throws SkillException
+     *             on file or specification consistency errors
+     */
+    public static SkillState open(File path, Mode... mode) throws IOException, SkillException {
+        assert path.exists() : "can only open files that already exist in genarel, because of java.nio restrictions";
+        return open(path.toPath(), mode);
     }
-    for (p ← pools) {
-      val fieldMap = p.fields.map { _.name }.zip(p.fields).toMap
 
-      for (n ← p.knownFields if !fieldMap.contains(n)) {
-        p.addKnownField(n, eliminatePreliminaryTypesIn)
-      }
+    /**
+     * Create a new skill file based on argument path and mode.
+     * 
+     * @throws IOException
+     *             on IO and mode related errors
+     * @throws SkillException
+     *             on file or specification consistency errors
+     * @note suppress unused warnings, because sometimes type declarations are
+     *       created, although nobody is using them
+     */
+    @SuppressWarnings("unused")
+    public static SkillState open(Path path, Mode... mode) throws IOException, SkillException {
+        ActualMode actualMode = new ActualMode(mode);
+        switch (actualMode.open) {
+        case Create:
+            // initialization order of type information has to match file parser
+            // and can not be done in place
+            StringPool strings = new StringPool(null);
+            ArrayList<StoragePool<?, ?>> types = new ArrayList<>(1);
+            Annotation annotation = new Annotation(types);
+            StringType stringType = new StringType(strings);
+
+            // create type information${
+      var i = -1
+      (for (t ← IR)
+        yield s"""
+            ${name(t)}Access ${name(t)} = new ${name(t)}Access(${i += 1; i}${
+        if (null == t.getSuperType) ""
+        else { ", "+name(t.getSuperType) }
+      });
+            types.add(${name(t)});"""
+      ).mkString("")
     }
-  }
+            return new SkillState(strings, types, path, actualMode.close);
 
-  // TODO type restrictions
-  def check : Unit = for (p ← pools.par; f ← p.allFields) try { f.check } catch {
-    case e : SkillException ⇒ throw new SkillException(s"check failed in $${p.name}.$${f.name}:\\n  $${e.getMessage}", e)
-  }
+        case Read:
+            return FileParser.read(FileInputStream.open(path), actualMode.close);
 
-  def flush : Unit = {
-    check;
-    mode match {
-      case Write  ⇒ new StateWriter(this, FileOutputStream.write(path))
-      case Append ⇒ new StateAppender(this, FileOutputStream.append(path))
+        default:
+            throw new IllegalStateException("should never happen");
+        }
     }
-  }
 
-  def close : Unit = {
-    flush;
-    // TODO invalidate state?
-  }
-
-  def changePath(newPath : Path) : Unit = mode match {
-    case Write                       ⇒ path = newPath
-    case Append if (path == newPath) ⇒ //nothing to do
-    case Append ⇒
-      Files.deleteIfExists(newPath);
-      Files.copy(path, newPath);
-      path = newPath
-  }
-
-  def changeMode(writeMode : Mode) : Unit = (mode, writeMode) match {
-    case (Append, Write) ⇒ mode = Write
-    case (Write, Append) ⇒
-      throw new IllegalArgumentException(
-        "Cannot change write mode from Write to Append, try to use open(<path>, Create, Append) instead."
-      )
-    case _ ⇒
-  }
-}
-
-object State {
-  /**
-   * Opens a file and sets correct modes. This may involve reading data from the file.
-   */
-  def open(path : Path, modes : Seq[Mode]) : State = {
-    // determine open mode
-    // @note read is preferred over create, because empty files are legal and the file has been created by now if it did not exist yet
-    // @note write is preferred over append, because usage is more inuitive
-    val openMode = modes.collect {
-      case m : OpenMode ⇒ m
-    }.ensuring(_.size <= 1, throw new IOException("You can either create or read a file.")).headOption.getOrElse(Read)
-    val writeMode = modes.collect {
-      case m : WriteMode ⇒ m
-    }.ensuring(_.size <= 1, throw new IOException("You can either write or append to a file.")).headOption.getOrElse(Write)
-
-    // create the state 
-    openMode match {
-      case Create ⇒
-
-        // initialization order of type information has to match file parser and can not be done in place
-        val strings = new StringPool(null)
-        val types = new ArrayBuffer[StoragePool[_ <: SkillType, _ <: SkillType]](2);
-        val annotation = Annotation(types)
-        val stringType = StringType(strings)
-
-        // create type information
+    public SkillState(StringPool strings, ArrayList<StoragePool<?, ?>> types, Path path, Mode mode) {
+        super(path, mode);
+        this.types = types;
+        poolByName = new HashMap<>();
+        for (StoragePool<?, ?> p : types)
+            poolByName.put(p.name(), p);
 ${
       var i = -1
       (for (t ← IR)
-        yield s"""        val ${name(t)} = new ${name(t)}StoragePool(stringType, annotation, ${i += 1; i}${
-        if (null == t.getSuperType) ""
-        else { ", "+name(t.getSuperType) }
-      })
-        types += ${name(t)}"""
-      ).mkString("\n")
+        yield s"""
+        ${name(t)}s = (${name(t)}Access) poolByName.get("${t.getSkillName}");
+"""
+      ).mkString("")
     }
-        new State(${
-      if (largeSpecificationMode) ""
-      else (for (t ← IR) yield s"""
-          ${name(t)},""").mkString
+        // finalize pools
+        for (StoragePool<?, ?> p : types) {
+            // @note this loop must happen in type order!
+
+            // set owners
+            if (p instanceof BasePool<?>)
+                ((BasePool<?>) p).setOwner(this);
+
+            // add missing field declarations
+            HashSet<String> fieldNames = new HashSet<>();
+            for (FieldDeclaration<?, ?> f : p.fields())
+                fieldNames.add(f.name());
+
+            for (String n : p.knownFields) {
+                if (!fieldNames.contains(n))
+                    p.addKnownField(n);
+            }
+        }
     }
-          strings,
-          types.toArray,
-          path,
-          writeMode
-        )
-      case Read ⇒
-        FileParser.read(FileInputStream.open(path), writeMode)
+
+    public SkillState(HashMap<String, StoragePool<?, ?>> poolByName, StringPool strings,
+            ArrayList<StoragePool<?, ?>> types, Path path, Mode mode) {
+        super(path, mode);
+        this.types = types;
+        this.poolByName = poolByName;
+${
+      var i = -1
+      (for (t ← IR)
+        yield s"""
+        ${name(t)}s = (${name(t)}Access) poolByName.get("${t.getSkillName}");
+"""
+      ).mkString("")
     }
-  }
+
+        // finalize pools
+        for (StoragePool<?, ?> p : types) {
+            // @note this loop must happen in type order!
+
+            // set owners
+            if (p instanceof BasePool<?>)
+                ((BasePool<?>) p).setOwner(this);
+
+            // add missing field declarations
+            HashSet<String> fieldNames = new HashSet<>();
+            for (FieldDeclaration<?, ?> f : p.fields())
+                fieldNames.add(f.name());
+
+            for (String n : p.knownFields) {
+                if (!fieldNames.contains(n))
+                    p.addKnownField(n);
+            }
+        }
+    }
+${
+      var i = -1
+      (for (t ← IR)
+        yield s"""
+    private final ${name(t)}Access ${name(t)}s;
+
+    @Override
+    public ${name(t)}Access ${name(t)}s() {
+        return ${name(t)}s;
+    }
+"""
+      ).mkString("")
+    }
+    @Override
+    public Iterable<? extends Access<? extends SkillObject>> allTypes() {
+        return types;
+    }
 }
 """)
 
     out.close()
-  }
-
-  private def fieldType(t : Type) : String = t match {
-    case t : Declaration             ⇒ s"""userTypes("${t.getSkillName}")"""
-
-    case t : GroundType              ⇒ t.getSkillName.capitalize+"Info"
-
-    case t : ConstantLengthArrayType ⇒ s"new ConstantLengthArrayInfo(${t.getLength}, ${fieldType(t.getBaseType)})"
-    case t : VariableLengthArrayType ⇒ s"new VariableLengthArrayInfo(${fieldType(t.getBaseType)})"
-    case t : ListType                ⇒ s"new ListInfo(${fieldType(t.getBaseType)})"
-    case t : SetType                 ⇒ s"new SetInfo(${fieldType(t.getBaseType)})"
-    case t : MapType                 ⇒ s"new MapInfo(${t.getBaseTypes.map(fieldType(_)).mkString("List(", ",", ")")})"
   }
 }
