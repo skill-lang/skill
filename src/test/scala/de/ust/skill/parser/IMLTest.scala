66d75022458dfe8175ae67576eb27b23ba7760fe
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2015-04-30 12:18:18 +0200

    added guard to IML test

--- src/test/scala/de/ust/skill/parser/IMLTest.scala
+++ src/test/scala/de/ust/skill/parser/IMLTest.scala
@@ -7,40 +7,43 @@ import scala.language.implicitConversions
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
-import de.ust.skill.generator;
+import de.ust.skill.generator
 import de.ust.skill.main.CommandLine
+import java.nio.file.Files
 
 @RunWith(classOf[JUnitRunner])
 class IMLTest extends FunSuite {
-  // TODO ignore all testes iff the file is not presenet
   val filename = new File("/home/feldentm/Desktop/iml.sf/specification/iml-all.skill")
 
-  ignore("parse iml.sf") {
-    assert(100 < Parser.process(filename).allTypeNames.size)
-  }
+  if (Files.exists(filename.toPath)) {
 
-  ignore("create statistics") {
-    CommandLine.main(Array("-p", "iml", "-L", "statistics", "/home/feldentm/Desktop/iml.sf/specification/iml-all.skill",
-      "/home/feldentm/Desktop/iml.sf/generated"))
-  }
+    test("parse iml.sf") {
+      assert(100 < Parser.process(filename).allTypeNames.size)
+    }
 
-  ignore("create doxygen") {
-    CommandLine.main(Array("-p", "iml", "-L", "doxygen", "/home/feldentm/Desktop/iml.sf/specification/iml-all.skill",
-      "/home/feldentm/Desktop/iml.sf/generated"))
-  }
+    test("create statistics") {
+      CommandLine.main(Array("-p", "iml", "-L", "statistics", "/home/feldentm/Desktop/iml.sf/specification/iml-all.skill",
+        "/home/feldentm/Desktop/iml.sf/generated"))
+    }
 
-  ignore("create java") {
-    CommandLine.main(Array("-p", "iml", "-L", "java", "-O@java:suppressWarnings=true", "/home/feldentm/Desktop/iml.sf/specification/iml-all.skill",
-      "/home/feldentm/Desktop/iml.sf/generated"))
-  }
+    test("create doxygen") {
+      CommandLine.main(Array("-p", "iml", "-L", "doxygen", "/home/feldentm/Desktop/iml.sf/specification/iml-all.skill",
+        "/home/feldentm/Desktop/iml.sf/generated"))
+    }
 
-  ignore("create scala") {
-    CommandLine.main(Array("-p", "iml", "-L", "scala", "/home/feldentm/Desktop/iml.sf/specification/iml-all.skill",
-      "/home/feldentm/Desktop/iml.sf/generated"))
+    ignore("create ada") {
+      CommandLine.main(Array("-p", "iml", "-L", "ada", "/home/feldentm/Desktop/iml.sf/specification/iml-all.skill",
+        "/home/feldentm/Desktop/iml.sf/generated"))
+    }
+
+    test("create java") {
+      CommandLine.main(Array("-p", "iml", "-L", "java", "-O@java:suppressWarnings=true", "/home/feldentm/Desktop/iml.sf/specification/iml-all.skill",
+        "/home/feldentm/Desktop/iml.sf/generated"))
+    }
+
+    test("create scala") {
+      CommandLine.main(Array("-p", "iml", "-L", "scala", "/home/feldentm/Desktop/iml.sf/specification/iml-all.skill",
+        "/home/feldentm/Desktop/iml.sf/generated"))
+    }
   }
-  //
-  //  test("create ada") {
-  //    generator.ada.Main.main(Array("/home/feldentm/Desktop/iml.sf/specification/iml-all.skill",
-  //      "/home/feldentm/Desktop/iml.sf/generated/scala"))
-  //  }
 }
