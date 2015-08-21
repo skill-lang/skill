350949e9d00df582e2d2eb5326751e27a3cf99f9
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2015-08-21 12:03:05 +0200

    renamed generic directories to help jenkins parse error messages

--- src/test/scala/de/ust/skill/generator/common/GenericTests.scala
+++ src/test/scala/de/ust/skill/generator/common/GenericTests.scala
@@ -49,8 +49,8 @@ abstract class GenericTests extends FunSuite with BeforeAndAfterAll {
         f.listFiles.filterNot(_.isDirectory)
 
     val targets = (
-      collect(new File(base, "<all>"))
-      ++ collect(if (new File(base, name).exists) new File(base, name) else new File(base, "<empty>"))
+      collect(new File(base, "[[all]]"))
+      ++ collect(if (new File(base, name).exists) new File(base, name) else new File(base, "[[empty]]"))
     ).filter(_.getName.endsWith(".sf")).sortBy(_.getName)
 
     targets.partition(_.getPath.contains("accept"))
