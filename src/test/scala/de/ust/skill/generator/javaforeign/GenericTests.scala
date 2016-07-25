d83ab3ae76a2658e298858ca087b439494cfe78a
Constantin Weißer <i7c@posteo.de>
2016-07-25 10:50:15 +0200

    [jforeign] Fix formatting in GenericTests

--- src/test/scala/de/ust/skill/generator/javaforeign/GenericTests.scala
+++ src/test/scala/de/ust/skill/generator/javaforeign/GenericTests.scala
@@ -5,31 +5,32 @@
 \*                                                                            */
 package de.ust.skill.generator.javaforeign
 
-import org.scalatest.FunSuite
 import java.io.File
-import de.ust.skill.main.CommandLine
+
 import scala.collection.mutable.ArrayBuffer
-import java.nio.file.Files
-import org.scalatest.Assertions
+import scala.reflect.io.Path.jfile2path
+
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.ConfigMap
-import scala.io.Codec
+import org.scalatest.FunSuite
+
+import de.ust.skill.main.CommandLine
 
 class GenericTests extends FunSuite with BeforeAndAfterAll {
 
   val testOnly = ""
 
-  def language : String = "javaforeign"
+  def language: String = "javaforeign"
 
-  def languageOptions : ArrayBuffer[String] = ArrayBuffer()
+  def languageOptions: ArrayBuffer[String] = ArrayBuffer()
 
-  def deleteOutDir(out : String) {
+  def deleteOutDir(out: String) {
     import scala.reflect.io.Directory
     Directory(new File("testsuites/javaForeign/src/main/java/", out)).deleteRecursively
   }
 
-  def makeGenBinaryTests(name : String) : Unit = {
-    
+  def makeGenBinaryTests(name: String): Unit = {
+
   }
 
   /**
@@ -37,23 +38,22 @@ class GenericTests extends FunSuite with BeforeAndAfterAll {
    *
    * @return (accept, reject)
    */
-  final def collectBinaries(name : String) : (Seq[File], Seq[File]) = {
+  final def collectBinaries(name: String): (Seq[File], Seq[File]) = {
     val base = new File("src/test/resources/genbinary")
-    def collect(f : File) : Seq[File] =
+    def collect(f: File): Seq[File] =
       (for (path ← f.listFiles if path.isDirectory) yield collect(path)).flatten ++
         f.listFiles.filterNot(_.isDirectory)
 
     val targets = (
       collect(new File(base, "[[all]]"))
-      ++ collect(if (new File(base, name).exists) new File(base, name) else new File(base, "[[empty]]"))
-    ).filter(_.getName.endsWith(".sf")).sortBy(_.getName)
+      ++ collect(if (new File(base, name).exists) new File(base, name) else new File(base, "[[empty]]"))).filter(_.getName.endsWith(".sf")).sortBy(_.getName)
 
     targets.partition(_.getPath.contains("accept"))
   }
 
-  def finalizeTests() : Unit = { } 
+  def finalizeTests(): Unit = {}
 
-  final def makeTest(path: File, name : String, mappingFile: File, skillFilePath : String) = test("generic: "+name) {
+  final def makeTest(path: File, name: String, mappingFile: File, skillFilePath: String) = test("generic: " + name) {
     deleteOutDir(name)
 
     CommandLine.exit = { s ⇒ throw (new Error(s)) }
@@ -70,8 +70,8 @@ class GenericTests extends FunSuite with BeforeAndAfterAll {
     makeGenBinaryTests(name)
   }
 
-  implicit class Regex(sc : StringContext) {
-    def r = new util.matching.Regex(sc.parts.mkString, sc.parts.tail.map(_ ⇒ "x") : _*)
+  implicit class Regex(sc: StringContext) {
+    def r = new util.matching.Regex(sc.parts.mkString, sc.parts.tail.map(_ ⇒ "x"): _*)
   }
 
   for (path ← new File("src/test/resources/javaForeign").listFiles() if path.isDirectory() && path.getName.endsWith(testOnly)) {
@@ -80,18 +80,17 @@ class GenericTests extends FunSuite with BeforeAndAfterAll {
     assert(mappingFile.exists(), s"Mapping file not found in ${path.getAbsolutePath}")
     val skillSpec = new File(path.getAbsolutePath + s"/$baseName.skill")
     val skillSpecPath = if (skillSpec.exists()) skillSpec.getPath else "-"
-      
-    
+
     try {
       makeTest(path, baseName, mappingFile, skillSpecPath)
     } catch {
-      case e : MatchError ⇒
+      case e: MatchError ⇒
         println(s"failed processing of $path:")
         e.printStackTrace(System.out)
     }
   }
 
-  override def afterAll(configMap : ConfigMap) {
+  override def afterAll(configMap: ConfigMap) {
     finalizeTests
   }
 }
