1437cd7168cc68e224fe696bf050bb3abdb9bf92
Constantin Weißer <i7c@posteo.de>
2016-10-24 02:15:25 +0200

    [jforeign] Fix unbound partial mappings

--- src/main/scala/de/ust/skill/jforeign/mapping/UnboundMappingRule.scala
+++ src/main/scala/de/ust/skill/jforeign/mapping/UnboundMappingRule.scala
@@ -7,6 +7,7 @@ import de.ust.skill.ir.UserType
 import de.ust.skill.jforeign.typing.TargetTypeExists
 import de.ust.skill.jforeign.typing.FieldAccessible
 import de.ust.skill.jforeign.typing.FieldMappedOnce
+import de.ust.skill.ir.Field
 
 class UnboundMappingRule(javaTypeName: String, fieldNames: List[String], total: Boolean) extends MappingRule {
 
@@ -16,11 +17,14 @@ class UnboundMappingRule(javaTypeName: String, fieldNames: List[String], total: 
   override def getJavaTypeName(): String = javaTypeName;
 
   def bind(skill: TypeContext, java: TypeContext): List[TypeRule] = {
-    if (!total) throw new RuntimeException("Not implemented: non-total unbound mappings");
-
     val javaType = java.get(javaTypeName)
+    val allFields = javaType.asInstanceOf[UserType].getFields
+    val mappedFields : List[Field] = if (total) allFields.toList else {
+      val fields: Set[String] = fieldNames.toSet
+      allFields.filter { fn => fields.contains(fn.getSkillName) }.toList
+    }
     List(new TargetTypeExists(javaType)) ++
-      javaType.asInstanceOf[UserType].getFields.map { f => new FieldAccessible(javaType, f) }.toList ++
-      javaType.asInstanceOf[UserType].getFields.map { f => new FieldMappedOnce(javaType, f) }.toList
+      mappedFields.map { f => new FieldAccessible(javaType, f) }.toList ++
+      mappedFields.map { f => new FieldMappedOnce(javaType, f) }.toList
   }
 }
