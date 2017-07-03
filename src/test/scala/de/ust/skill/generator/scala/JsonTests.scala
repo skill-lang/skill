330260d2919982742bd7ff0e89ff4c2bec638bf3
vonzampr <vonzampr@studi.informatik.uni-stuttgart.de>
2017-07-03 16:18:18 +0200

    Make skillfile-names in scala tests different

--- src/test/scala/de/ust/skill/generator/scala/JsonTests.scala
+++ src/test/scala/de/ust/skill/generator/scala/JsonTests.scala
@@ -99,7 +99,7 @@ class Generic${name}Test extends CommonTest {
     def testname = new File(testfile).getName.replace(".json", "");
     rval.write(s"""
 	test("${packagePath} - ${testname}") {
-    val path = tmpFile("write.generic");
+    val path = tmpFile("write.generic.${testname}");
     val sf = SkillFile.open(path, Create, Write);
     reflectiveInit(sf);
     
