bd5cdf7aa26a532c4fe64b7c310fb193f5154019
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2017-10-25 11:10:43 +0200

    upgraded skill project to scala 2.12

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "skill"
 
 version := "0.9"
 
-scalaVersion := "2.11.8"
+scalaVersion := "2.12.4"
 
 javacOptions ++= Seq("-encoding", "UTF-8")
 
--- src/main/java/de/ust/skill/ir/UserType.java
+++ src/main/java/de/ust/skill/ir/UserType.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Definition of a regular user type.
