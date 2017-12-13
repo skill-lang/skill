005e4821ea3b5d5c0fcefba211a2510247d1a7ac
Jan Rapp <janrapp@live.de>
2017-12-13 17:27:07 +0100

    Load and check SkillFiles in generated java tests

--- src/test/scala/de/ust/skill/generator/java/APITests.scala
+++ src/test/scala/de/ust/skill/generator/java/APITests.scala
@@ -106,6 +106,7 @@ public class Generic${name}Test extends common.CommonTest {
   }
 
   def makeRegularTest(out : PrintWriter, kind : String, name : String, testName : String, accept : Boolean, tc : TypeContext, obj : JSONObject) {
+    try { // TODO remove try
     out.write(s"""
     @Test${if (accept) "" else "(expected = SkillException.class)"}
     public void APITest_${escaped(kind)}_${name}_${if (accept) "acc" else "fail"}_${escaped(testName)}() throws Exception {
@@ -114,9 +115,18 @@ public class Generic${name}Test extends common.CommonTest {
         // create objects${createObjects(obj, tc, name)}
         // set fields${setFields(obj, tc)}
 
+        { // read back and assert correctness
+            SkillFile sf2 = SkillFile.open(sf.currentPath(), Mode.Read, Mode.ReadOnly);
+            // check count per Type${createCountChecks(obj, tc, name)}
+            // create objects from file${createObjects2(obj, tc, name)}
+            // assert fields${assertFields(obj, tc)}
+        }
         sf.close();
     }
 """)
+    } catch {
+      case e : Throwable => e.printStackTrace()
+    }
   }
 
   private def typ(tc : TypeContext, name : String) : String = {
@@ -140,9 +150,39 @@ public class Generic${name}Test extends common.CommonTest {
     }
   }
 
-  private def value(v : Any, f : Field) : String = value(v, f.getType)
+  private def equalValue(left : String, v : Any, f : Field) : String = equalValue(left, v, f.getType)
 
-  private def value(v : Any, t : Type) : String = t match {
+  private def equalValue(left : String, v : Any, t : Type) : String = t match {
+    case t : GroundType ⇒
+      t.getSkillName match {
+        case "string" if null != v ⇒ s"""$left != null && $left.equals("${v.toString()}")"""
+        case "i8"                  ⇒ s"$left == (byte)" + v.toString()
+        case "i16"                 ⇒ s"$left == (short)" + v.toString()
+        case "f32"                 ⇒ s"$left == (float)" + v.toString()
+        case "f64"                 ⇒ s"$left == (double)" + v.toString()
+        case "v64" | "i64"         ⇒ s"$left == " + v.toString() + "L"
+        case _ if null != v && !v.toString().equals("null") && !v.toString().equals("true") && !v.toString().equals("false")
+                                   ⇒ s"$left == " + v.toString() + "_2"
+        case _                     ⇒ s"$left == " + v.toString()
+      }
+
+    case t : SingleBaseTypeContainer ⇒
+      v.asInstanceOf[JSONArray].iterator().toArray.map(value(_, t.getBaseType, "_2")).mkString(t match {
+        case t : ListType ⇒ s"$left != null && $left.equals(list("
+        case t : SetType  ⇒ s"$left != null && $left.equals(set("
+        case _            ⇒ s"$left != null && $left.equals(array("
+      }, ", ", "))").replace("java.util.", "")
+
+    case t : MapType if v != null ⇒ s"$left != null && $left.equals(" + valueMap(v.asInstanceOf[JSONObject], t.getBaseTypes.toList, "_2") + ")"
+
+    case _ ⇒
+      if (v == null || v.toString().equals("null")) s"$left == (${mapType(t)}) null"
+      else s"$left == ${v.toString()}_2"
+  }
+
+  //private def value(v : Any, f : Field, suffix : String = "") : String = value(v, f.getType, suffix)
+
+  private def value(v : Any, t : Type, suffix : String = "") : String = t match {
     case t : GroundType ⇒
       t.getSkillName match {
         case "string" if null != v ⇒ s""""${v.toString()}""""
@@ -151,37 +191,86 @@ public class Generic${name}Test extends common.CommonTest {
         case "f32"                 ⇒ "(float)" + v.toString()
         case "f64"                 ⇒ "(double)" + v.toString()
         case "v64" | "i64"         ⇒ v.toString() + "L"
+        case _ if null != v        ⇒ v.toString() + suffix
         case _                     ⇒ v.toString()
       }
 
     case t : SingleBaseTypeContainer ⇒
-      v.asInstanceOf[JSONArray].iterator().toArray.map(value(_, t.getBaseType)).mkString(t match {
+      v.asInstanceOf[JSONArray].iterator().toArray.map(value(_, t.getBaseType, suffix)).mkString(t match {
         case t : ListType ⇒ "list("
         case t : SetType  ⇒ "set("
         case _            ⇒ "array("
       }, ", ", ")").replace("java.util.", "")
 
-    case t : MapType if v != null ⇒ valueMap(v.asInstanceOf[JSONObject], t.getBaseTypes.toList)
+    case t : MapType if v != null ⇒ valueMap(v.asInstanceOf[JSONObject], t.getBaseTypes.toList, suffix)
 
     case _ ⇒
       if (v == null || v.toString().equals("null")) s"(${mapType(t)}) null"
-      else v.toString()
+      else v.toString() + suffix
   }
 
-  private def valueMap(v : Any, ts : List[Type]) : String = {
+  private def valueMap(v : Any, ts : List[Type], suffix : String = "") : String = {
     if (1 == ts.length) {
-      value(v, ts.head)
+      value(v, ts.head, suffix)
     } else {
       var rval = s"map()"
       val obj = v.asInstanceOf[JSONObject]
 
+      // https://docs.scala-lang.org/overviews/collections/maps.html#operations-in-class-map
+      // ms put (k, v) Adds mapping from key k to value v to ms and returns any value previously associated with k as an option.
       for (name ← JSONObject.getNames(obj)) {
-        rval = s"put($rval, ${value(name, ts.head)}, ${valueMap(obj.get(name), ts.tail)})"
+        rval = s"put($rval, ${value(name, ts.head, suffix)}, ${valueMap(obj.get(name), ts.tail, suffix)})"
       }
 
       rval;
     }
   }
+
+  private def createCountChecks(obj : JSONObject, tc : TypeContext, packagePath : String) : String = {
+    if (null == JSONObject.getNames(obj)) {
+      ""
+    } else {
+
+      val objCountPerType = scala.collection.mutable.Map[String, Int]()
+      for (name ← JSONObject.getNames(obj)) {
+        val x = obj.getJSONObject(name)
+        val t = JSONObject.getNames(x).head;
+
+        val typeName = typ(tc, t);
+        if (!(objCountPerType contains typeName)) {
+          objCountPerType += (typeName -> 0)
+        }
+        objCountPerType(typeName) = objCountPerType(typeName) + 1
+      }
+
+      val rval = for ((typeName, objCount) <- objCountPerType) yield {
+        s"""
+            assert(sf.${typeName}s().staticSize() == ${objCount});"""
+      }
+
+      rval.mkString
+    }
+  }
+
+  private def createObjects2(obj : JSONObject, tc : TypeContext, packagePath : String) : String = {
+    if (null == JSONObject.getNames(obj)) {
+      ""
+    } else {
+
+      val rval = for (name ← JSONObject.getNames(obj)) yield {
+        val x = obj.getJSONObject(name)
+        val t = JSONObject.getNames(x).head;
+
+        val typeName = typ(tc, t);
+
+        s"""
+            $packagePath.$typeName ${name}_2 = sf2.${typeName}s().getByID($name.getSkillID());"""
+      }
+
+      rval.mkString
+    }
+  }
+
 
   private def createObjects(obj : JSONObject, tc : TypeContext, packagePath : String) : String = {
     if (null == JSONObject.getNames(obj)) {
@@ -199,6 +288,35 @@ public class Generic${name}Test extends common.CommonTest {
       }
 
       rval.mkString
+    }
+  }
+
+  private def assertFields(obj : JSONObject, tc : TypeContext) : String = {
+    if (null == JSONObject.getNames(obj)) {
+      ""
+    } else {
+
+      val rval = for (name ← JSONObject.getNames(obj)) yield {
+        val x = obj.getJSONObject(name)
+        val t = JSONObject.getNames(x).head;
+        val fs = x.getJSONObject(t);
+
+        if (null == JSONObject.getNames(fs))
+          ""
+        else {
+          val assignments = for (fieldName ← JSONObject.getNames(fs).toSeq) yield {
+            val f = field(tc, t, fieldName)
+            val getter = escaped("get" + f.getName.capital())
+
+            s"""
+            assert(${equalValue(s"${name}_2.$getter()",fs.get(fieldName), f)});"""
+          }
+
+          assignments.mkString
+        }
+      }
+
+      rval.mkString("\n")
     }
   }
 
@@ -220,7 +338,7 @@ public class Generic${name}Test extends common.CommonTest {
             val setter = escaped("set" + f.getName.capital())
 
             s"""
-        $name.$setter(${value(fs.get(fieldName), f)});"""
+        $name.$setter(${value(fs.get(fieldName), f.getType)});"""
           }
 
           assignments.mkString
